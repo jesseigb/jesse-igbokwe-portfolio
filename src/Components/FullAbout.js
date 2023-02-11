f65e28a749a35d111075efb2585d96af9db3df61
jesseigb <77441355+jesseigb@users.noreply.github.com>
2023-02-11 23:05:28 +0000

    Font Sizing

--- src/Components/FullAbout.js
+++ src/Components/FullAbout.js
@@ -33,13 +33,7 @@ function FullAbout() {
         <Container className='full-window' fluid>
             <Row className='window-content full'>
                 <Row className="window-header">
-                    <Col className="window-action-wrapper" style={{paddingLeft: '30px'}} xs={4}>
-                        <div className="close">
-                            <img src={closeIcon} draggable="false"></img>
-                        </div>
-                        <div className="minimise">
-                            <img src={minimiseIcon} draggable="false"></img>
-                        </div>
+                    <Col className="window-action-wrapper" style={{paddingLeft: '30px'}} xs={3}>
                         <div onClick={() => goToPage('')} className="shrink">
                             <img src={shrinkIcon} draggable="false"></img>
                         </div>
