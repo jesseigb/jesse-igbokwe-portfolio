ffe8b8e9eb6ca906e835aecf3fea41159f445b7a
jesseigb <77441355+jesseigb@users.noreply.github.com>
2023-02-15 00:02:22 +0000

    Navigation Adjustement and Contact Page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,93 @@
 import './App.scss';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Work from './Components/Work';
 import {Container, Row, Col} from 'react-bootstrap';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import FullWork from './Components/FullWork';
 import FullAbout from './Components/FullAbout';
+import FullContact from './Components/FullContact';
 
 function App() {
+  const navigate = useNavigate();
+
+    const goToPage = (page) => {
+        navigate('/' + page);
+    };
 
   //Switch CSS styling and close appropriate window based on navbar button state
   const [isWorkActive, setWorkActive] = useState('false');
   const [isAboutActive, setAboutActive] = useState('false');
   const [isContactMeActive, setContactMeActive] = useState('false');
 
-  // Depending on which navbar button has been clicked switch boolean
-  const handleToggle = (buttonName) => {
-    if(buttonName == 'work') {
-      setWorkActive(!isWorkActive);
+  //Check window width of the user's browser
+  const [width, setWidth] = useState(window.innerWidth);
+
+  //Check browser windows size:
+  //Detect if user is using a desktop, tablet or mobile device
+  const detectWindowSize = () => {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', detectWindowSize);
+    return () => {
+      window.removeEventListener('resize', detectWindowSize);
     }
-    else if(buttonName == 'about') {
-      setAboutActive(!isAboutActive);
+  }, []);
+
+  let isMobile = width <= 850;
+  
+  const handleNavigation = (buttonName) => {
+    
+    const navbarButtons = document.querySelector('.nav-bar-wrapper').childNodes;
+    
+    //Mobile Navigation:
+    //Only allow one button to be active and direct to appropriate page 
+    if(isMobile) {
+
+      //Toggle navbar CSS active styling and redirect
+      if(buttonName == 'about') {
+        navbarButtons[0].classList.remove('navbar-active');
+        navbarButtons[1].classList.remove('navbar-active');
+        navbarButtons[2].classList.add('navbar-active');
+        goToPage('about');
+      }
+
+      if(buttonName == 'work') {
+        navbarButtons[0].classList.remove('navbar-active');
+        navbarButtons[1].classList.add('navbar-active');
+        navbarButtons[2].classList.remove('navbar-active');
+        goToPage('work');
+      }
+
+      if(buttonName == 'contact') {
+        navbarButtons[0].classList.add('navbar-active');
+        navbarButtons[1].classList.remove('navbar-active');
+        navbarButtons[2].classList.remove('navbar-active');
+
+        //Only navigate to contact page when browser is mobile
+        if(isMobile) {
+          goToPage('contact');
+        }
+      }
     }
-    else if(buttonName == 'contact') {
-      setContactMeActive(!isContactMeActive);
+
+    //Desktop Navigation:
+    //Simply switch boolean to make windows visible and button active
+    if(!isMobile) {
+      if(buttonName == 'work') {
+        setWorkActive(!isWorkActive);
+        if (isWorkActive ? navbarButtons[1].classList.add('navbar-active') : navbarButtons[1].classList.remove('navbar-active'));
+      }
+      else if(buttonName == 'about') {
+        setAboutActive(!isAboutActive);
+        if (!isAboutActive ? navbarButtons[2].classList.add('navbar-active') : navbarButtons[2].classList.remove('navbar-active'));
+      }
+      else if(buttonName == 'contact') {
+        setContactMeActive(!isContactMeActive);
+        if (isContactMeActive ? navbarButtons[0].classList.toggle('navbar-active') : navbarButtons[0].classList.remove('navbar-active'));
+      }
     }
   };
 
@@ -33,21 +96,33 @@ function App() {
       <Container className="header-container" fluid>
         <Row>
               <Col className="nav-bar-wrapper" xs={12}>
-                <div style={{marginRight: '0px'}} onClick={() => handleToggle('contact')} className={!isContactMeActive ? "navbar-active" : null}><span>contact me</span></div>
-                <div onClick={() => handleToggle('work')} className={!isWorkActive ? "navbar-active" : null}><span>work</span></div>
-                <div onClick={() => handleToggle('about')} className={isAboutActive ? "navbar-active" : null}><span>about</span></div>
+                <div 
+                  style={{marginRight: '0px'}} 
+                  onClick={() => handleNavigation('contact')}>
+                  <span>contact me</span>
+                </div>
+
+                <div 
+                  onClick={() => handleNavigation('work')}>
+                  <span>work</span>
+                </div>
+
+                <div 
+                  className='navbar-active'
+                  onClick={() => handleNavigation('about')}>
+                  <span>about</span>
+                </div>
               </Col>
         </Row>
       </Container>
 
       <div className="desktop-wrapper">
-        <Router>
           <Routes>
-            <Route path='/' element={[<About active={isAboutActive}/>,<Contact active={!isContactMeActive}/>,<Work active={!isWorkActive}/>]}></Route>
+            <Route path='/' element={isMobile ? <FullAbout /> : [<About active={isAboutActive}/>,<Contact active={!isContactMeActive}/>,<Work active={!isWorkActive}/>]}></Route>
             <Route path='/work' element={<FullWork/>}></Route>
             <Route path='/about' element={<FullAbout/>}></Route>
+            <Route path='/contact' element={<FullContact/>}></Route>
           </Routes>
-        </Router>
       </div>
     </div>
   );
---
+++ src/Components/FullContact.js
@@ -0,0 +1,41 @@
+import {useState, React} from 'react';
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import Journey from './JourneyText';
+import leftArrow from '../assets/img/left-arrow.png';
+
+function FullAbout() {
+
+    const navigate = useNavigate();
+
+    const goToPage = (page) => {
+        navigate('/' + page);
+    };
+
+    return (
+        <Container className='full-window' fluid>
+            <Row className='window-content full'>
+                <Row className="window-header">
+                    <img onClick={() => goToPage('')} src={leftArrow} draggable="false"></img>
+                    <p>Contact Me</p>
+                </Row>
+                <Col className='contact-content' lg={6} xs={12}>
+                    <div>
+                        <h1>Get In Touch</h1>
+                        <p>If you wanna get in touch, talk to me about a project collaboration or just say hello 
+                            send me an email or contact me on my LinkedIn.</p>
+                        <div className='bubble-title'>
+                            <p><a href='mailto: jesse.igbokwe@hotmail.com'>Email</a></p>
+                        </div>
+                        <br></br>
+                        <div className='bubble-title'>
+                            <p><a href='https://www.linkedin.com/in/jesse-igbokwe-79341b203/'>LinkedIn</a></p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default FullAbout;
