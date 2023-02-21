a881f57e45ac9b8ba81bc9f48f17a9555211fa5c
jesseigb <jesse.igbokwe@hotmail.com>
2023-02-21 12:39:38 +0000

    Texts

--- src/Components/FullAbout.js
+++ src/Components/FullAbout.js
@@ -94,7 +94,7 @@ function FullAbout() {
                             <div onClick={() => setJourneyObject({
                                 title: 'Skills City', 
                                 subTitle: 'Software Engineering Bootcamp', 
-                                description: "Thanks to this experience I expanded knowledge on web development tools like React. Also, during this bootcamp I was in a team with a client where I was resposible for the development of a booking managment web-based system for a private surgery by adopting key web technologies like Python-based framework Django and Github.",
+                                description: "This experience helped me expand knowledge on JS framework React. Also, during this bootcamp I was in a team with a client where I was resposible for the development of a booking managment web-based system for a private surgery by adopting key web technologies like Python-based framework Django and Github.",
                                 skills: ['html', 'css', 'javascript', 'react', 'python', 'django-white', 'github', 'bootstrap']})} 
                                 className='timeline-fourth-point'></div>
                         </div>
